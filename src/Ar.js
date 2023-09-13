480c7d53189fa566f07d2a0b62d7f6eaaf28c66a
gonzalolionelprieto <gonzalolionelprieto@gmail.com>
2023-09-12 23:25:20 -0300

    Primeros pasos con R.A , implementando camara

--- src/Ar.js
+++ src/Ar.js
@@ -7,7 +7,7 @@ function Ar() {
         <a-assets>
           <a-asset-item 
           id="target-file"
-           src="../descarga.zpt" />
+           src="https://cdn.glitch.global/f10276b0-f2eb-4cc5-ae61-86afbaa05b4e/descarga.zpt?v=1694571895016" />
         </a-assets>
         <a-entity zappar-image="#target-file">
           <a-box color="red" rotation="0 45 45" scale="1.2 1.2 1.2"></a-box>
