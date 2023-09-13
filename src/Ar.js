e2e63a71666a58755664595df4a07b32eaadfa8c
gonzalolionelprieto <gonzalolionelprieto@gmail.com>
2023-09-12 23:30:44 -0300

    Primeros pasos con R.A , implementando camara

--- src/Ar.js
+++ src/Ar.js
@@ -9,6 +9,11 @@ function Ar() {
           id="target-file"
            src="https://cdn.glitch.global/f10276b0-f2eb-4cc5-ae61-86afbaa05b4e/descarga.zpt?v=1694571895016" />
         </a-assets>
+       
+        <a-entity zappar-permissions-ui id="permissions">
+   
+        <a-entity text="value: Please reload the page, accepting the camera permissions." position="0 0 -2"></a-entity>
+        </a-entity>
         <a-entity zappar-image="#target-file">
           <a-box color="red" rotation="0 45 45" scale="1.2 1.2 1.2"></a-box>
         </a-entity>
